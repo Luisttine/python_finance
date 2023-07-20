72fd09f49852b79a4ba278de7b68ed4a85ad5c70
Luisouza <luisttine@gmail.com>
2023-07-20 16:23:20 -0300

    migration, base_html and view for category created

--- finance_project/app_finance/models.py
+++ finance_project/app_finance/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class PurchaseCategory(models.Model):
+    id = models.AutoField(primary_key=True)
+    category_name = models.TextField(max_length=255)
+    testeeee = models.TextField(max_length=255)
+
--- finance_project/app_finance/views.py
+++ finance_project/app_finance/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import PurchaseCategory
 
-# Create your views here.
+def home(request):
+    return render(request, 'home/home.html')
+
+def add_category(request):
+    new_category = PurchaseCategory()
+    new_category.category_name = request.POST.get('nome')
+    new_category.save()
+    return render(request, 'category/add_category.html')
+
+def category(request):
+    
+    category = {
+        'category': PurchaseCategory.objects.all()
+    }
+
+    return render(request, 'category/list_category.html', category)
